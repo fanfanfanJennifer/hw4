6703fe6ee6e88c6f3ce277e19330df616b98ce63
Yifan Zhang <116487190+fanfanfanJennifer@users.noreply.github.com>
2024-04-20 14:33:49 -0500

    Update hw4.sh

--- hw4.sh
+++ hw4.sh
@@ -2,13 +2,15 @@
 
 # untar your R installation
 tar -xzf R413.tar.gz
-tar -xzf packages_FITSio.tar.gz
-tar -xzf $2.tgz
+tar -xzf packages.tar.gz
 
 export PATH=$PWD/R/bin:$PATH
 export RHOME=$PWD/R
 export R_LIBS=$PWD/packages
 
+tar -xvzf $2.tgz
 
 # run your script
 Rscript hw4.R $1 $2 
+
+
