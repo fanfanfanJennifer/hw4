df742f9641a7058aae649f4848e09daf2b4ba8b1
Yifan Zhang <116487190+fanfanfanJennifer@users.noreply.github.com>
2024-04-19 23:26:36 -0500

    Update hw4.sh

--- hw4.sh
+++ hw4.sh
@@ -5,7 +5,6 @@ tar -xzf R413.tar.gz
 tar -xzf packages_FITSio.tar.gz
 # If selecting next line, also change "transfer_input_files" line in
 # myscript.sub to transfer the file.
-#
 # tar -xzf packages_FITSio_tidyverse.tar.gz
 
 # make sure the script will use your R installation, 
@@ -17,5 +16,4 @@ export R_LIBS=$PWD/packages
 tar -xzf $2.tgz
 
 # run your script
-Rscript hw4.R $1 $2 # note: the two actual command-line arguments
-                         # are in myscript.sub's "arguments = " line
+Rscript hw4.R $1 $2 
